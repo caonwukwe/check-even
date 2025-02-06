13c2d9e452af8e7eabf802c884a23f53001a3aa6
Chinazom Onwukwe <caonwukwe@gmail.com>
2025-02-06 21:52:06 +0000

    check even completed

---
+++ even.py
@@ -0,0 +1,13 @@
+def check_even(number):
+
+    if number % 2 == 0:
+        
+        print(f"{number} True")
+    else:
+        print(f"{number} False")
+
+
+print(check_even(100))
+print(check_even(1777))
+
+
