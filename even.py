a8e448b4f1e21128b479ca0f7ef1b6385779ff5c
Chinazom Onwukwe <caonwukwe@gmail.com>
2025-02-13 21:01:20 +0000

    return added

--- even.py
+++ even.py
@@ -2,10 +2,11 @@ def check_even(number):
 
     if number % 2 == 0:
         
-        print(f"{number} True")
-    else:
-        print(f"{number} False")
+        return True
 
+    else:
+        return False
+    
 
 print(check_even(100))
 print(check_even(1777))
